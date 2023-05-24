4194b70520e98a35bbff59062ab335b2a323a164
Vivian Vu <vhlananhjr@gmail.com>
2023-05-24 15:15:35 -0400

    File-based routing

---
+++ pages/about.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/Home.module.css";
+
+export default function AboutPage() {
+  return (
+    <>
+      <h1 className={styles.heading}>This is ABOUT page!</h1>
+    </>
+  );
+}
---
+++ pages/admin.tsx
@@ -0,0 +1,9 @@
+import styles from "@/styles/Home.module.css";
+
+export default function AdminPage() {
+  return (
+    <>
+      <h1 className={styles.heading}> This is ADMIN page</h1>
+    </>
+  );
+}
